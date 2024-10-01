3a39e764a643de9c7e23ebec37c2f93693f96488
muhammed-gumus <mr.silver.mg@gmail.com>
2024-10-01 16:51:48 +0300

    feat:added divider

--- client/app/page.js
+++ client/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
+
 export default function Home() {
   return (
-    <div className="flex flex-col max-w-screen-xl mx-auto min-h-screen">
+    <div className="flex flex-col gap-12 max-w-screen-xl mx-auto min-h-screen">
       <Hero />
+      {/* Yatay çizgi */}
+      <hr className="border-t-1 border-gray-400 my-4" />
       <Features />
     </div>
   );
