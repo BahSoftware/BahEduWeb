4146a0574f83e494ccf61754044d60380c04f088
muhammed-gumus <mr.silver.mg@gmail.com>
2024-10-01 14:38:32 +0300

    feat:created Feature component

--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import Features from "@/components/Features";
 export default function Home() {
   return (
-    <div className="flex max-w-screen-xl mx-auto min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col max-w-screen-xl mx-auto min-h-screen">
       <Hero />
+      <Features />
     </div>
   );
 }
