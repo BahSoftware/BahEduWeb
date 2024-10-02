673545b91f95ff89c2e1d21c2ac1dec64ab6f71e
muhammed-gumus <mr.silver.mg@gmail.com>
2024-10-02 17:20:47 +0300

    Değişiklikleri kaydet

--- server/api/main.js
+++ server/api/main.js
@@ -13,6 +13,10 @@ app.use(cors());
 const registerRoutes = require('../register/register'); // register.js dosyasını içe aktar
 app.use('/register', registerRoutes); // Kullanıcı kayıt rotalarını '/register' yoluna bağla
 
+const loginRoutes = require('../register/login'); // register.js dosyasını içe aktar
+app.use('/login', loginRoutes); // Kullanıcı kayıt rotalarını '/register' yoluna bağla
+
+
 // Kök dizine basit bir yanıt ver
 app.get('/', (req, res) => {
     res.send('API çalışıyor!'); // Basit bir mesaj döndür
--- server/register/login.js
+++ server/register/login.js
@@ -1,36 +1,42 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-require('dotenv').config({ path: '../.env' });
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config({ path: "../../.env" });
 
 const router = express.Router();
 
 // MongoDB Atlas bağlantı URL'sini .env dosyasından al
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 // Giriş için endpoint
-router.post('/', async (req, res) => {
-    const { email, password } = req.body;
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
 
-    try {
-        await client.connect();
-        const db = client.db("myDatabase");
-        const collection = db.collection("users");
+  try {
+    await client.connect();
+    const db = client.db("myDatabase");
+    const collection = db.collection("users");
 
-        // Kullanıcıyı sorgula
-        const user = await collection.findOne({ email: email, password: password });
+    // Kullanıcıyı sorgula
+    const user = await collection.findOne({ email: email, password: password });
 
-        if (user) {
-            res.status(200).send({ message: "Giriş başarılı", user });
-        } else {
-            res.status(401).send({ message: "Geçersiz e-posta veya şifre" });
-        }
-    } catch (error) {
-        console.error("Giriş hatası:", error);
-        res.status(500).send("Giriş hatası");
-    } finally {
-        await client.close();
+    if (user) {
+      res.status(200).send({ message: "Giriş başarılı", user });
+    } else {
+      res.status(401).send({ message: "Geçersiz e-posta veya şifre" });
     }
+  } catch (error) {
+    console.error("Giriş hatası:", error);
+    res.status(500).send("Giriş hatası");
+  } finally {
+    await client.close();
+  }
 });
 
 module.exports = router;
